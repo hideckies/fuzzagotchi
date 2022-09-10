b6b3ec0eb11c15b350c28682c739cdab7abf544c
hideckies <hideckies@gmail.com>
2022-09-11 00:36:43 +0900

    added worker but not implemented yet

--- libgotchi/worker.go
+++ libgotchi/worker.go
@@ -1,10 +1,25 @@
 package libgotchi
 
+// Mutex -> https://gobyexample.com/mutexes
+
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-func Work() {
+type Worker struct {
+	mtx      sync.Mutex
+	counters map[string]int
+}
+
+func (w *Worker) inc(name string) {
+	w.mtx.Lock()
+	defer w.mtx.Unlock()
+	w.counters[name]++
+}
+
+func Worker1() {
 	jobs := make(chan int, 5)
 	done := make(chan bool)
 
@@ -29,4 +44,82 @@ func Work() {
 	fmt.Println("sent all jobs")
 
 	<-done
+}
+
+// worker pool
+func Worker2(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "started job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2
+	}
+}
+
+func workerPool() {
+	const numJobs = 5
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go Worker2(w, jobs, results)
+	}
+
+	for j := 1; j <= numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	for a := 1; a <= numJobs; a++ {
+		<-results
+	}
+}
+
+// wait group
+func Worker3(id int) {
+	fmt.Printf("Worker %d starting\n", id)
+
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+
+func waitgroup() {
+	var wg sync.WaitGroup
+
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+
+		i := i
+
+		go func() {
+			defer wg.Done()
+			Worker3(i)
+		}()
+	}
+
+	wg.Wait()
+}
+
+// mutex
+func mutex() {
+	w := Worker{
+		counters: map[string]int{"a": 0, "b": 0},
+	}
+
+	var wg sync.WaitGroup
+
+	doIncrement := func(name string, n int) {
+		for i := 0; i < n; i++ {
+			w.inc(name)
+		}
+		wg.Done()
+	}
+
+	wg.Add(3)
+	go doIncrement("a", 1000)
+	go doIncrement("a", 1000)
+	go doIncrement("b", 1000)
+
+	wg.Wait()
+	fmt.Println(w.counters)
 }
